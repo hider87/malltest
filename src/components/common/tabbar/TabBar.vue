8f4d9702ac082afe35364b5642fea7ffd6cb783b
hider87 <1357584714@qq.com>
2021-11-01 18:48:39 +0800

    init

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="tar-bar">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+}
+</script>
+
+<style scoped>
+  @import "~assets/css/base.css";
+  #tar-bar {
+    display: flex;
+    background-color: #f6f6f6;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    box-shadow: 0 -3px 1px rgba(100,100,100,0.01);
+  }
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img slot="item-icon" src="../assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="../assets/img/tabbar/category_active.svg">
+      <div slot="item-text">
+        首页
+      </div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="../assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="../assets/img/tabbar/category_active.svg">
+      <div slot="item-text">
+        首页
+      </div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="../assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="../assets/img/tabbar/category_active.svg">
+      <div slot="item-text">
+        首页
+      </div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="../assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="../assets/img/tabbar/category_active.svg">
+      <div slot="item-text">
+        首页
+      </div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "./tabbar/TabBar";
+import TabBarItem from "./tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
